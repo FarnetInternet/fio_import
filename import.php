01e89fdf43b9f169868ba49006ca3212249bfc19
Farnet Internet <109293362+FarnetInternet@users.noreply.github.com>
2024-03-30 13:43:37 +0100

    new features
    New responsive design

--- import.php
+++ import.php
@@ -1,11 +1,60 @@
 <?php
 require_once 'FioApi.php';
 
+// Function to create database and table
+function createDatabaseAndTable($servername, $username, $password, $dbname) {
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Create database
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+    if ($conn->query($sql) === TRUE) {
+        echo "Database created successfully or already exists<br>";
+    } else {
+        echo "Error creating database: " . $conn->error;
+        return false;
+    }
+
+    // Select database
+    $conn->select_db($dbname);
+
+    // Create table
+    $sql = "CREATE TABLE IF NOT EXISTS platbyTest (
+      `transactionId` bigint NOT NULL,
+      `datum` date DEFAULT NULL,
+      `objem` decimal(10,2) DEFAULT NULL,
+      `mena` varchar(3) DEFAULT NULL,
+      `protiucet` varchar(50) DEFAULT NULL,
+      `kodBanky` varchar(4) DEFAULT NULL,
+      `nazevBanky` varchar(100) DEFAULT NULL,
+      `typ` varchar(100) DEFAULT NULL,
+      `provedl` varchar(100) DEFAULT NULL,
+      `idPokynu` bigint DEFAULT NULL,
+      `konstantnySymbol` varchar(10) DEFAULT NULL,
+      `variabilnySymbol` varchar(10) DEFAULT NULL,
+      `specificSymbol` varchar(10) DEFAULT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Table created successfully or already exists<br>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+        return false;
+    }
+
+    $conn->close();
+    return true;
+}
+
 // Získání proměnných z formuláře v index.php
 $token = $_GET['token'];
 $dateFrom = $_GET['dateFrom'];
 $dateTo = $_GET['dateTo'];
-//$variableSymbol = $_POST['variableSymbol'];
 
 // Připojovací údaje k databázi
 $servername = $_GET['servername'];
@@ -26,31 +75,14 @@ $response = file_get_contents($url);
 if ($response !== false) {
     $data = json_decode($response, true);
 
-    // Vytvoření SQL tabulky
-    $sql = "CREATE TABLE IF NOT EXISTS platbyTest (
-  `transactionId` bigint NOT NULL,
-  `datum` date DEFAULT NULL,
-  `objem` decimal(10,2) DEFAULT NULL,
-  `mena` varchar(3) DEFAULT NULL,
-  `protiucet` varchar(50) DEFAULT NULL,
-  `kodBanky` varchar(4) DEFAULT NULL,
-  `nazevBanky` varchar(100) DEFAULT NULL,
-  `typ` varchar(100) DEFAULT NULL,
-  `provedl` varchar(100) DEFAULT NULL,
-  `idPokynu` bigint DEFAULT NULL,
-  `konstantnySymbol` varchar(10) DEFAULT NULL,
-  `variabilnySymbol` varchar(10) DEFAULT NULL,
-  `specificSymbol` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;";
+    // Create database and table
+    if (createDatabaseAndTable($servername, $username, $password, $dbname)) {
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
 
-    // Připojení k databázi
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Připojení k databázi zlyhalo: " . $conn->connect_error);
-    }
-
-    // Vytvoření tabulky
-    if ($conn->query($sql) === TRUE) {
         // Vložení/importování platby do tabulky
         $startTime = microtime(true); // Začátek importu
         $importedCount = 0;
@@ -65,18 +97,18 @@ if ($response !== false) {
             $nazevBanky = isset($transaction['column12']) ? $transaction['column12']['value'] : '';
             $typ = $transaction['column10']['value'];
             $provedl = isset($transaction['column8']) ? $transaction['column8']['value'] : '';
-            $idPokynu = intval($row['column17']['value']);
+            $idPokynu = intval($transaction['column17']['value']);
 
-            $varSymbol = isset($transaction['column4']) ? $transaction['column4']['value'] : ''; // Získanie hodnoty konštantného symbolu (KS)
+            $variabilnySymbol = isset($transaction['column4']) ? $transaction['column4']['value'] : '';
 
-            $insertSql = "INSERT INTO platbyTest (transactionId, datum, objem, mena, protiucet, kodBanky, nazevBanky, typ, provedl, idPokynu, varSymbol)
-                          VALUES ('$transactionId', '$datum', '$objem', '$mena', '$protiucet', '$kodBanky', '$nazevBanky', '$typ', '$provedl', '$idPokynu', '$varSymbol')";
+            $insertSql = "INSERT INTO platbyTest (transactionId, datum, objem, mena, protiucet, kodBanky, nazevBanky, typ, provedl, idPokynu, variabilnySymbol)
+                          VALUES ('$transactionId', '$datum', '$objem', '$mena', '$protiucet', '$kodBanky', '$nazevBanky', '$typ', '$provedl', '$idPokynu', '$variabilnySymbol')";
 
-            if ($conn->query($insertSql) === TRUE) {
+            if ($conn->query($insertSql) === true) {
                 $importedCount++;
             } else {
                 echo '<div class="alert alert-warning">Chyba při importování platby: ' . $conn->error . '</div>';
-                exit;
+                exit();
             }
         }
 
@@ -87,17 +119,17 @@ if ($response !== false) {
         echo '<div class="alert alert-info" role="alert">Čas trvání importu: ' . round($importDuration, 2) . ' sekund</div>';
 
         // Save debug info to a file
-        $debugInfo = "Import start time: " . date("Y-m-d H:i:s", $startTime) . "\n";
-        $debugInfo .= "Import end time: " . date("Y-m-d H:i:s", $endTime) . "\n";
-        $debugInfo .= "Import duration: " . round($importDuration, 2) . " seconds\n";
-        $debugInfo .= "Number of imported payments: " . $importedCount . "\n";
+        $debugInfo = "Čas zahájení importu: " . date("Y-m-d H:i:s", $startTime) . "\n";
+        $debugInfo .= "Čas ukončení importu: " . date("Y-m-d H:i:s", $endTime) . "\n";
+        $debugInfo .= "Doba trvání importu: " . round($importDuration, 2) . " seconds\n";
+        $debugInfo .= "Počet importovaných plateb: " . $importedCount . "\n";
 
         file_put_contents('debug.log', $debugInfo, FILE_APPEND | LOCK_EX); // Create or append to the debug.log file
-    } else {
-        echo '<div class="alert alert-danger">Chyba při importování platby: ' . $conn->error . '</div>';
-    }
 
-    $conn->close();
+        $conn->close();
+    } else {
+        echo "Nepodařilo se vytvořit databázi a tabulku.";
+    }
 } else {
     echo 'Chyba při získávání dat z Fio API.';
 }
--- index.php
+++ index.php
@@ -1,110 +1,104 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Automatický import plateb</title>
-    <!-- Pripojenie Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	<link href="app.css" rel="stylesheet" >
-    <!-- Pripojenie Bootstrap a jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container mt-4">
-        <h1>Automatický import plateb</h1>
-        <hr>
-        <div id="current-payment"></div>
-        <div id="result"></div>
-        <!-- Formulář pro vstupní údaje -->
-        <div class="row">
-            <div class="col-md-6">
-                <form id="importForm" action="import.php">
-                    <div class="py-2"></div>
-                    <h4>Připojení k FIO API</h4>
-                    <hr>
-                    <div class="form-group">
-                        <label for="tokenInput">FIO API token:</label>
-                        <input type="text" class="form-control" id="tokenInput" name="token">
-                    </div>
-					<div class="mb-3"></div>
-                    <div class="form-group">
-                        <label for="dateFromInput">Datum od:</label>
-                        <input type="date" class="form-control" id="dateFromInput" name="dateFrom">
-                    </div>
-					<div class="mb-3"></div>
-                    <div class="form-group">
-                        <label for="dateToInput">Datum do:</label>
-                        <input type="date" class="form-control" id="dateToInput" name="dateTo" value="<?php echo date('d.m.Y'); ?>">
-                    </div>
-
-					<div class="mb-3"></div>
-                </form>
+<html lang="cs">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Automatický import plateb</title>
+        <!-- Připojení Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="app.css" rel="stylesheet" />
+        <style>
+            /* Vlastní styly */
+            body {
+                background-color: #f8f9fa;
+            }
+            .container {
+                max-width: 600px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <h1 class="text-center">Automatický import plateb</h1>
+            <hr />
+            <div id="result"></div>
+            <!-- Formulář pro vstupní údaje -->
+            <div class="row">
+                <div class="col-md-6">
+                    <form id="importForm">
+                        <div class="mb-3">
+                            <label for="tokenInput" class="form-label">FIO API token:</label>
+                            <input type="text" class="form-control" id="tokenInput" name="token" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="dateFromInput" class="form-label">Datum od:</label>
+                            <input type="date" class="form-control" id="dateFromInput" name="dateFrom" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="dateToInput" class="form-label">Datum do:</label>
+                            <input type="date" class="form-control" id="dateToInput" name="dateTo" value="<?php echo date('Y-m-d'); ?>" />
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <!-- Formulář pro připojení k databázi -->
+                    <form id="databaseForm">
+                        <div class="mb-3">
+                            <label for="servernameInput" class="form-label">IP/Adresa serveru:</label>
+                            <input type="text" class="form-control" id="servernameInput" name="servername" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="usernameInput" class="form-label">Uživatelské jméno:</label>
+                            <input type="text" class="form-control" id="usernameInput" name="username" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="passwordInput" class="form-label">Heslo:</label>
+                            <input type="password" class="form-control" id="passwordInput" name="password" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="dbnameInput" class="form-label">Název databáze:</label>
+                            <input type="text" class="form-control" id="dbnameInput" name="dbname" />
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="col-md-6">
-                <!-- Formulář pro připojení k databázi -->
-                <div class="py-2"></div>
-                <h4>Připojení k databázi</h4>
-                <hr>
-                <form id="databaseForm" action="import.php">
-                    <div class="form-group">
-                        <label for="servernameInput">IP/Adresa serveru:</label>
-                        <input type="text" class="form-control" id="servernameInput" name="servername">
-                    </div>
-					<div class="mb-3"></div>
-                    <div class="form-group">
-                        <label for="usernameInput">Uživatelské jméno:</label>
-                        <input type="text" class="form-control" id="usernameInput" name="username">
-                    </div>
-					<div class="mb-3"></div>
-                    <div class="form-group">
-                        <label for="passwordInput">Heslo:</label>
-                        <input type="password" class="form-control" id="passwordInput" name="password">
-                    </div>
-					<div class="mb-3"></div>
-                    <div class="form-group">
-                        <label for="dbnameInput">Název databáze:</label>
-                        <input type="text" class="form-control" id="dbnameInput" name="dbname">
-                    </div>
-                </form>
+            <div class="text-center">
+                <button id="submitButton" class="btn btn-primary">Odeslat</button>
             </div>
         </div>
-		<div class="py-3"></div>
-        <center>
-            <button id="submitButton" class="btn btn-primary">Odeslat</button>
-        </center>
-    </div>
 
-    <script>
-        $(document).ready(function() {
-            // Po kliknutí na tlačítko "Odeslat"
-            $('#submitButton').click(function(e) {
-                e.preventDefault();
+        <!-- Připojení Bootstrap a jQuery -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script>
+            $(document).ready(function () {
+                // Po kliknutí na tlačítko "Odeslat"
+                $("#submitButton").click(function (e) {
+                    e.preventDefault();
 
-                // Získání dat z obou formulářů
-                var importFormData = $('#importForm').serialize();
-                var databaseFormData = $('#databaseForm').serialize();
+                    // Získání dat z obou formulářů
+                    var importFormData = $("#importForm").serialize();
+                    var databaseFormData = $("#databaseForm").serialize();
 
-                // Spojení dat z obou formulářů
-                var formData = importFormData + '&' + databaseFormData;
+                    // Spojení dat z obou formulářů
+                    var formData = importFormData + "&" + databaseFormData;
 
-                // AJAX požadavek na import.php
-                $.ajax({
-                    url: 'import.php',
-                    type: 'GET',
-                    data: formData,
-                    success: function(response) {
-                        // Zobrazení odpovědi
-                        $('#result').html(response);
-                    },
-                    error: function(xhr, status, error) {
-                        // Zobrazení chyby
-                        console.log(error);
-                    }
+                    // AJAX požadavek na import.php
+                    $.ajax({
+                        url: "import.php",
+                        type: "GET",
+                        data: formData,
+                        success: function (response) {
+                            // Zobrazení odpovědi
+                            $("#result").html(response);
+                        },
+                        error: function (xhr, status, error) {
+                            // Zobrazení chyby
+                            console.log(error);
+                        },
+                    });
                 });
             });
-        });
-    </script>
-</body>
+        </script>
+    </body>
 </html>
